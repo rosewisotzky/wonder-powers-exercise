2dbcf0b939a9157b9b01055cc8842daa1ba2fbbc
Rose Wisotzky <rose@rosewisotzky.com>
2019-03-08 10:14:19 -0600

    Got all three individual event handlers to work

--- src/scripts/powers.js
+++ src/scripts/powers.js
@@ -6,11 +6,30 @@ console.log("good morning friend how does that coffee taste? like learning lots 
 // Write a function named flightHandlerFunction that will remove the disabled class on the corresponding <section id="flight"> (section, not button) and replace it with a class of enabled.
 // Have your developer tools open. When you click the button, the following element...
 let newFlight = document.querySelector("#flight")
-const flightHandlerFunction = (newClass) => {
-    newFlight.classList.toggle(newClass)
-}
+let newMind = document.querySelector("#mindreading")
+let xRay = document.querySelector("#xray")
 
+const flightHandlerFunction = (newClass) => {
+    newFlight.classList.toggle(newClass);
+    newFlight.classList.toggle("disabled");
+
+}
+const mindReadHandlerFunction = (newClass) => {
+    newMind.classList.toggle(newClass);
+    newMind.classList.toggle("disabled");
+
+}
+const xRayHandlerFunction = (newClass) => {
+    xRay.classList.toggle(newClass);
+    xRay.classList.toggle("disabled");
+
+}
 document.querySelector("#activate-flight").addEventListener("click", function () {
     flightHandlerFunction("enabled")
 })
-
+document.querySelector("#activate-mindreading").addEventListener("click", function () {
+    mindReadHandlerFunction("enabled")
+})
+document.querySelector("#activate-xray").addEventListener("click", function () {
+    xRayHandlerFunction("enabled")
+})
